cf3de7fc32b97a024455ade6ba3a43c7561899fa
Stephen Murphy <stephen@devhaus.ie>
2015-10-16 15:06:16 +0100

    Test
    Test

--- footer.php
+++ footer.php
@@ -24,14 +24,14 @@
     Spletna stran Mozilla Slovenija je na voljo pod okriljem dovoljenja <a href="http://creativecommons.org/licenses/by/2.5/si/" rel="external license">Creative Commons Priznanje avtorstva 2.5 Slovenija.</a><br /><br />
     <a target="_blank" href="mailto:info@mozilla.si?Subject=Mozilla Slovenija" rel="external">Stopite v stik z nami</a>
     </p>
-   
+
     <div id="footermenu">
     <nav id="nav-meta">
 		<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('footermenu')) : else : ?>
 		<?php endif; ?>
         </nav>
 	</div>
-    
+
     <nav id="nav-social">
       <ul role="navigation">
         <li><a target="_blank" href="https://www.facebook.com/mozillaslovenija" rel="home">Facebook</a></li>
@@ -39,7 +39,7 @@
         <li><a target="_blank" href="http://www.youtube.com/user/mozillaslovenija" rel="external">Youtube</a></li>
       </ul>
     </nav>
-    
+
   </div>
 </footer>
 
